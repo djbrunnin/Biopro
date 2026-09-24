2c6e826734d88c939842138b2cc0f953a5cb48e6
djbrunnin <57035670+djbrunnin@users.noreply.github.com>
2026-09-24 00:53:16 -0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://tgqetjuxmttnljtuaawm.supabase.co";
-const SUPABASE_KEY = "sb_publishable_iQrxBgUGYxkjtmmhC-zrcg_eiAFzkvL";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
